9f0653b5a45ab3dd6f8c64a097b3dff1680f6a9d
Muhammad-Hassaan92 <mhassaan.786.909@gmail.com>
2025-07-09 14:38:53 +0500

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div class="container-fluid">
-            <div class="row my-3 mx-2 rounded bg-primary" id='header'>
-                <div class="col-md-7 d-flex flex-column justify-content-center px-5">
-                    <div class="display-1 fw-bolder text-light">MEN'S</div>
-                    <div class="display-1 fw-bolder text-dark">Shoe Collections</div>
-                </div>
-                <div class="col-5">
-                    <img class="img-fluid py-5" src="https://muhammad-hassaan92.github.io/Ecommerce-Landing-Page-Shoes/images/shoe1.png" alt="Shoe"></img>
-                </div>
+        <div class="row my-3 mx-2 rounded bg-primary" id='header'>
+            <div class="col-md-7 d-flex flex-column justify-content-center px-5">
+                <div class="display-1 fw-bolder text-light">MEN'S</div>
+                <div class="display-1 fw-bolder text-dark">Shoe Collections</div>
+            </div>
+            <div class="col-5">
+                <img class="img-fluid py-5" src="https://muhammad-hassaan92.github.io/Ecommerce-Landing-Page-Shoes/images/shoe1.png" alt="Shoe"></img>
             </div>
         </div>
     )
